0f1ebb1d8fd230bfd69a5191d485fcc3179af956
Sven Speckmaier <sps@ipunkt.biz>
2018-07-16 20:16:54 +0200

    excluded lxml from build as it uses c parts

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ cp -r -L ${DIR}/floating_ip "${FLOATING_IP_DIR}"
 cd "${DIR}/build/floating_ip_build"
 sudo python3 -m pip install hetznercloud --target .
 sudo python3 -m pip install ifaddr --upgrade --target .
-sudo python3 -m pip install lxml --upgrade --target .
 python3 -m zipapp -p '/usr/bin/env python3' --output "${DIR}/build/floating_ip" "${FLOATING_IP_DIR}"
 
 STONITH_DIR="${DIR}/build/stonith_build"
@@ -21,7 +20,6 @@ cp -r -L "${DIR}/stonith" "${STONITH_DIR}"
 cd "${DIR}/build/stonith_build"
 sudo python3 -m pip install hetznercloud --target .
 sudo python3 -m pip install ifaddr --upgrade --target .
-sudo python3 -m pip install lxml --upgrade --target .
 python3 -m zipapp -p '/usr/bin/env python3' --output "${DIR}/build/hetzner_cloud" "${STONITH_DIR}"
 
 cd ${DIR}
