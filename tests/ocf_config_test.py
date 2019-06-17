b0fae8a509b430551d0c1887749c58595a473da9
Sven Speckmaier <sps@ipunkt.biz>
2019-06-17 09:17:44 +0200

    added config test

---
+++ tests/ocf_config_test.py
@@ -0,0 +1,12 @@
+import unittest
+import unittest.mock
+import ocf.config
+
+class OcfConfigTest(unittest.TestCase):
+
+    @unittest.mock.patch('os.environ')
+    def setUp(self, environ):
+        self.ocfConfig = ocf.config.Config(environ)
+
+    def testOcfConfigExists(self):
+        ocfConfig = ocf.config.Config()
